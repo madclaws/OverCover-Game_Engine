e8c6bb26f0e1a38dbdee62567da2e583f0912a9a
madclaws <pavanan.anandu63@gmail.com>
2016-12-10 08:13:15 -0800

    shader class updation

--- OverCover/Shaders.cpp
+++ OverCover/Shaders.cpp
@@ -3,29 +3,29 @@
 
 Shaders::Shaders(const GLchar* vert_source,const GLchar* frag_source)
 {
-															//Initializing variables
-	GLuint VertexID,FragmentID;								//ID's for vertex and fragment shaders
-	GLint sucess;											//To store the compile status
-	GLchar infolog[512];									//compile logs
+	//...........Initializing variables....................................................
+	GLuint VertexID,FragmentID;				//ID's for vertex and fragment shaders
+	GLint sucess;							//To store the compile status
+	GLchar infolog[512];					//compile logs
 	//............ COMPILING FRAGMENT SHADER ....................................................										
-	VertexID=glCreateShader(GL_VERTEX_SHADER);						//Creating Vertex shader ID
-	glShaderSource(VertexID,1,&vert_source,0);						//Loading Vertex shader Source
-	glCompileShader(VertexID);										//Compiling Vertex shader
-	glGetShaderiv(VertexID,GL_COMPILE_STATUS,&sucess);				//Retreiving Compile status
+	VertexID=glCreateShader(GL_VERTEX_SHADER);	//Creating Vertex shader ID
+	glShaderSource(VertexID,1,&vert_source,0);	//Loading Vertex shader Source
+	glCompileShader(VertexID);					//Compiling Vertex shader
+	glGetShaderiv(VertexID,GL_COMPILE_STATUS,&sucess);	//Retreiving Compile status
 	if(!sucess)
 	{
-		glGetShaderInfoLog(VertexID,512,0,infolog);					//Getting infolog
+		glGetShaderInfoLog(VertexID,512,0,infolog);			//Getting infolog
 		cout<<"FAILED COMPILING VERTEX SHADER ID::"<<VertexID<<endl;
 		cout<<infolog<<endl;
 		_sleep(2);
-		exit(0);													//Exiting if compilation failed
+		exit(0);								//Exiting if compilation failed
 	}
 	cout<<"SUCCESS COMPILING VERTEX SHADER ID::"<<VertexID<<endl;
 	//............ COMPILING FRAGMENT SHADER ....................................................
-	FragmentID=glCreateShader(GL_FRAGMENT_SHADER);					//Creating Fragment shader ID
-	glShaderSource(FragmentID,1,&frag_source,0);					//Loading Fragment shader Source
-	glCompileShader(FragmentID);									//Compiling Fragment shader
-	glGetShaderiv(FragmentID,GL_COMPILE_STATUS,&sucess);			//Retreiving Compile status
+	FragmentID=glCreateShader(GL_FRAGMENT_SHADER);			//Creating Fragment shader ID
+	glShaderSource(FragmentID,1,&frag_source,0);			//Loading Fragment shader Source
+	glCompileShader(FragmentID);							//Compiling Fragment shader
+	glGetShaderiv(FragmentID,GL_COMPILE_STATUS,&sucess);	//Retreiving Compile status
 	if(!sucess)
 	{
 		glGetShaderInfoLog(FragmentID,512,0,infolog);
